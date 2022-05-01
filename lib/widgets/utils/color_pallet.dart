23bc17fb8f793524612e6f31d625e8623c05b941
YoLoN <shadyolown13@onet.pl>
2022-05-01 18:19:58 +0200

    Add basic color pallet

---
+++ lib/widgets/utils/color_pallet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPallet {
+  static const Map<String, Color> colors = {
+    'primary': Color.fromRGBO(56, 200, 251, 1),
+    'secondary': Color.fromRGBO(40, 44, 90, 1),
+    'pink': Color.fromRGBO(196, 85, 244, 1),
+    'purple': Color.fromRGBO(94, 85, 214, 1),
+  };
+}
