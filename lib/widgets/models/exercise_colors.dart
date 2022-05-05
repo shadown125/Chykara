d655eaf0212c50a0b755fe0b3f04df4f1e9186ed
YoLoN <shadyolown13@onet.pl>
2022-05-05 17:43:44 +0200

    Add exercise colors model :sparkles:

---
+++ lib/widgets/models/exercise_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ExcerciseColors {
+  final Color color;
+
+  const ExcerciseColors(this.color);
+}
--- lib/widgets/utils/color_pallet.dart
+++ lib/widgets/utils/color_pallet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/exercise_colors.dart';
+
 class ColorPallet {
   static const Map<String, Color> colors = {
     'primary': Color.fromRGBO(56, 200, 251, 1),
@@ -7,6 +9,31 @@ class ColorPallet {
     'pink': Color.fromRGBO(196, 85, 244, 1),
     'purple': Color.fromRGBO(94, 85, 214, 1),
     'white': Colors.white,
+    'green': Color.fromRGBO(121, 220, 170, 1),
     'lightBlue': Color.fromRGBO(77, 210, 255, 1),
+    'lightOrange': Color.fromRGBO(255, 165, 128, 1),
   };
+
+  static const List<ExcerciseColors> exerciseColors = [
+    ExcerciseColors(Color.fromRGBO(56, 200, 251, 1)),
+    ExcerciseColors(Color.fromRGBO(255, 165, 128, 1)),
+    ExcerciseColors(Color.fromRGBO(196, 85, 244, 1)),
+    ExcerciseColors(Color.fromRGBO(94, 85, 214, 1)),
+    ExcerciseColors(Color.fromRGBO(121, 220, 170, 1)),
+    ExcerciseColors(Color.fromRGBO(56, 200, 251, 1)),
+    ExcerciseColors(Color.fromRGBO(255, 165, 128, 1)),
+    ExcerciseColors(Color.fromRGBO(196, 85, 244, 1)),
+    ExcerciseColors(Color.fromRGBO(94, 85, 214, 1)),
+    ExcerciseColors(Color.fromRGBO(121, 220, 170, 1)),
+    ExcerciseColors(Color.fromRGBO(56, 200, 251, 1)),
+    ExcerciseColors(Color.fromRGBO(255, 165, 128, 1)),
+    ExcerciseColors(Color.fromRGBO(196, 85, 244, 1)),
+    ExcerciseColors(Color.fromRGBO(94, 85, 214, 1)),
+    ExcerciseColors(Color.fromRGBO(121, 220, 170, 1)),
+    ExcerciseColors(Color.fromRGBO(56, 200, 251, 1)),
+    ExcerciseColors(Color.fromRGBO(255, 165, 128, 1)),
+    ExcerciseColors(Color.fromRGBO(196, 85, 244, 1)),
+    ExcerciseColors(Color.fromRGBO(94, 85, 214, 1)),
+    ExcerciseColors(Color.fromRGBO(121, 220, 170, 1)),
+  ];
 }
