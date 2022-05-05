a6235e632daa36af7383d2823e62b77891dcae80
YoLoN <shadyolown13@onet.pl>
2022-05-05 17:44:12 +0200

    Change dummy data for exercise :hammer:

--- lib/dummy_data_staging.dart
+++ lib/dummy_data_staging.dart
@@ -1,10 +1,8 @@
-const Map<String, Map<String, int>> dummyDataExercise = {
-  'stretching': {
-    'time': 10,
-    'burn': 80,
-  },
-  'deadlift': {
-    'time': 6,
-    'burn': 30,
-  }
-};
+import './widgets/models/exercises.dart';
+
+final List<Exercises> dummyDataExercise = [
+  Exercises(id: '1', title: 'stretching', time: 10, series: 3),
+  Exercises(id: '2', title: 'deadlift', time: 6, series: 3, kalories: 30),
+  Exercises(id: '3', title: 'wyciskanie', time: 16, series: 3, kalories: 50),
+  Exercises(id: '4', title: 'przysiady', time: 8, series: 3),
+];
