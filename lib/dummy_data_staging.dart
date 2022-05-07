8f9bc7fa5fc995f106af7d5b5aedbb7d5e9c7ead
YoLoN <shadyolown13@onet.pl>
2022-05-07 20:50:43 +0200

    Change dummy Exercise data structure :hammer:

--- lib/dummy_data_staging.dart
+++ lib/dummy_data_staging.dart
@@ -1,8 +1,34 @@
 import './widgets/models/exercises.dart';
 
 final List<Exercises> dummyDataExercise = [
-  Exercises(id: '1', title: 'stretching', time: 10, series: 3),
-  Exercises(id: '2', title: 'deadlift', time: 6, series: 3, kalories: 30),
-  Exercises(id: '3', title: 'wyciskanie', time: 16, series: 3, kalories: 50),
-  Exercises(id: '4', title: 'przysiady', time: 8, series: 3),
+  Exercises(
+    id: '1',
+    title: 'Stretching',
+    reps: 12,
+    time: 10,
+    series: 3,
+  ),
+  Exercises(
+    id: '2',
+    title: 'Deadlift',
+    time: 6,
+    reps: 8,
+    series: 3,
+    weight: 50,
+  ),
+  Exercises(
+    id: '3',
+    title: 'Wyciskanie',
+    time: 16,
+    reps: 10,
+    series: 3,
+    weight: 20,
+  ),
+  Exercises(
+    id: '4',
+    title: 'Przysiady',
+    time: 8,
+    reps: 16,
+    series: 3,
+  ),
 ];
