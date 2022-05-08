6869cf55085fd481defd2f28ee14c68592e64088
YoLoN <shadyolown13@onet.pl>
2022-05-08 18:25:33 +0200

    Add basic person model :sparkles:

---
+++ lib/widgets/models/person.dart
@@ -0,0 +1,13 @@
+class Person {
+  double weight;
+  String name;
+  int completedTrainings;
+  DateTime signUpDate;
+
+  Person({
+    required this.weight,
+    required this.name,
+    required this.completedTrainings,
+    required this.signUpDate,
+  });
+}
