b4bb320ca8f2387408da62dc00870b79f9ae2fec
YoLoN <shadyolown13@onet.pl>
2022-05-08 18:25:47 +0200

    Add dummy person data :sparkles:

---
+++ lib/dummy_data_person.dart
@@ -0,0 +1,10 @@
+import './widgets/models/person.dart';
+
+final List<Person> dummyPersonData = [
+  Person(
+    weight: 98.5,
+    name: 'John',
+    completedTrainings: 3,
+    signUpDate: DateTime.now(),
+  ),
+];
